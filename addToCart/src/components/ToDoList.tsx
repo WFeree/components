0a58ba7f06f523abce2275df199c3c1114d6fba1
WFeree - CAD1 <ivacyka12@gmail.com>
2025-09-16 14:29:05 +0200

    Todo +

--- addToCart/src/components/ToDoList.tsx
+++ addToCart/src/components/ToDoList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ToDoList(){
     const [todos, setTodos] = useState<string[]>(["Alma", "Körte"])
@@ -9,12 +9,20 @@ function ToDoList(){
         setText("")
     }
 
+    // useEffect(()=>{
+    //     console.log(text);
+    // },[text])
+
+    const removeFromTodo = (removeable: string) => {
+        setTodos(prev => prev.filter(e => e !== removeable))
+    }
+
     return(
         <>
             <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
             <button onClick={addToTodos}>+</button>
             <ul>
-                { todos.map(todo => <li>{todo}</li>)}
+                { todos.map(todo => <li>{todo} <button onClick={()=>removeFromTodo(todo)}>X</button></li> )}
             </ul>
         </>
     )
